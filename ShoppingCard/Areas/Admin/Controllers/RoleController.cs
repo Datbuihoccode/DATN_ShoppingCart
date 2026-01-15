55c1afccd32b3430d06f223079d7a98b382a24fe
Datbuihoccode <toro2k3@gmail.com>
2026-01-15 10:46:19 +0700

    CRUD Roles

---
+++ ShoppingCard/Areas/Admin/Controllers/RoleController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ShoppingCard.Models;
+using ShoppingCard.Repository;
+using System.Threading.Tasks;
+
+namespace ShoppingCard.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("Admin/Role")]
+    [Authorize(Roles = " Admin")]
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<AppUserModel> _userManager;
+        private readonly DataContext _dataContext;
+        public RoleController(DataContext context, RoleManager<IdentityRole> roleManager, UserManager<AppUserModel> userManager)
+        {
+            _dataContext = context;
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _dataContext.Roles.OrderByDescending(p => p.Id).ToListAsync());
+        }
+
+        // -- Create role --
+        [HttpGet]
+        [Route("Create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [Route("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            if (!_roleManager.RoleExistsAsync(role.Name).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(role.Name)).GetAwaiter().GetResult();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // -- Edit role --
+        [HttpGet]
+        [Route("Edit")]
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound(); // Handle null or empty id
+            }
+            var role =  await _roleManager.FindByIdAsync(id);
+            
+            return View();
+        }
+        [HttpPost]
+        [Route("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, IdentityRole model)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound(); // Handle null or empty id
+            }
+            if (ModelState.IsValid)
+            {
+                var role = await _roleManager.FindByIdAsync(id);
+                if (role == null)
+                {
+                    return NotFound(); // Handle role not found
+                }
+
+                role.Name = model.Name;
+
+                try
+                {
+                    await _roleManager.UpdateAsync(role);
+                    TempData["success"] = "Role đã được cập nhật.";
+                    return RedirectToAction("Index");
+                }
+                catch(Exception ex)
+                {
+                    ModelState.AddModelError("","Lỗi khi cập nhật Role. Vui lòng thử lại.");
+                }
+            }
+            return View(model ?? new IdentityRole { Id = id});
+        }
+
+        // -- Delete role --
+        [HttpGet]
+        [Route("Delete")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound(); // Handle null or empty id
+            }
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                return NotFound(); // Handle role not found
+            }
+
+            try
+            {
+                await _roleManager.DeleteAsync(role);
+                TempData["success"] = "Role đã được xóa.";
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError("","Lỗi khi xóa Role. Vui lòng thử lại.");
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- ShoppingCard/obj/Debug/net9.0/ShoppingCard.AssemblyInfo.cs
+++ ShoppingCard/obj/Debug/net9.0/ShoppingCard.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingCard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+431784cde89dd3031cf448e4d2e7e2f1aab4fd1b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0546b4a3688b62e08681b34de067afd13d6e9ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingCard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingCard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
